b3c4e269f4b6d777dd371496f898e5f72eab4a27
Tony L. Kerz <tony@WA716948A-HFD-OEM.local>
2015-06-09 13:32:27 -0400

    [webpack config]

--- app/app.js
+++ app/app.js
@@ -1,13 +1,5 @@
 import './app.scss'
 import module1 from './module-1/module-1'
-import debug_ from 'debug'
-import jq from 'jquery'
+import $ from 'jquery'
 
-var dbg = debug_('app:')
-var elt = jq('#myId')
-var domElt = elt[0];
-var getElt = document.getElementById('myId')
-
-dbg('jq=%o, elt=%o, dom-elt=%o, get-elt=%o', jq, elt, domElt, getElt)
-
-elt.append(module1)
+$('#myId').append(module1)
