2f8a7f19571b667881653f28e453107073991ccf
Tony L. Kerz <tony@WA716948A-HFD-OEM.local>
2015-06-10 15:44:42 -0400

    [webpack config]

--- app/app.js
+++ app/app.js
@@ -2,4 +2,9 @@ import './app.scss'
 import module1 from './module-1/module-1'
 import $ from 'jquery'
 
-$('#myId').append(module1)
+var myElt = $('#myId')
+if (!myElt.length) {
+  console.log('#myId not found, creating...')
+  myElt = $('<div />').appendTo('body')
+}
+myElt.append(module1)
--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,9 +1,24 @@
 var commonConfig = require('./webpack.common.config')
 var _ = require('lodash')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = _.extend(
+config = _.extend(
   commonConfig,
   {
-    devtool: 'cheap-module-eval-source-map'
+    devtool: 'cheap-module-eval-source-map',
+    debug: true,
+    plugins: [
+      new HtmlWebpackPlugin(
+        {
+          template: 'index.reload.html',
+          inject: 'body'
+          //reload: 'http://localhost:8080/webpack-dev-server.js'
+          //https://github.com/ampedandwired/html-webpack-plugin/pull/58
+        }
+      )
+    ]
   }
 )
+
+console.log('config=%j', config)
+module.exports = config
